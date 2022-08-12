cb4c83837144f80030b03f4a784d6a2674009635
lahirutech <rmllbrathnayaka@gmail.com>
2022-08-12 16:30:54 +0530

    adding the index page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import CategoryCard from '../components/CategoryCard';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <main>
-      <h1>Shopping cart</h1>
+    <main className={styles.container}>
+      <div className={styles.small}>
+        <CategoryCard image="https://imgur.com/uKQqsuA.png" name="Xbox" />
+        <CategoryCard image="https://imgur.com/3Y1DLYC.png" name="PS5" />
+        <CategoryCard image="https://imgur.com/Dm212HS.png" name="Switch" />
+      </div>
+      <div className={styles.large}>
+        <CategoryCard image="https://imgur.com/qb6IW1f.png" name="PC" />
+        <CategoryCard
+          image="https://imgur.com/HsUfuRU.png"
+          name="Accessories"
+        />
+      </div>
     </main>
-  )
-}
+  );
+};
+
+export default HomePage;
