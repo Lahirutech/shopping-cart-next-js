20d77f73518bdfecda4b25d98e14a13a80f14134
lahirutech <rmllbrathnayaka@gmail.com>
2022-08-14 23:21:20 +0530

    adding the data request

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,1 +1,17 @@
-    
+import data from "./data.json";
+
+export function getProducts() {
+  return data;
+}
+
+export default function (req, res) {
+  if (req.method !== "GET") {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).json({
+      message: `Method ${req.method} is not Allowed`,
+    });
+  } else {
+    const products = getProducts();
+    res.status(200).json(products)
+  }
+}
