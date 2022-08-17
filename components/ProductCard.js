7c1188a0aea1cf5d558fc433198e6682e77746a7
lahirutech <rmllbrathnayaka@gmail.com>
2022-08-17 18:32:43 +0530

    change product card css import

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,6 +1,9 @@
+import Image from 'next/image';
+import styles from '../styles/ProductCard.module.css';
+
 const ProductCard = ({ product }) => {
   return (
-    <div className={styles}>
+    <div className={styles.card}>
       <Image src={product.image} height={300} width={220} />
       <h4 className={styles.title}>{product.product}</h4>
       <h5 className={styles.category}>{product.category}</h5>
