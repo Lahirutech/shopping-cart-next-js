960a2cc19402632eea45b10fdcd1fd0cb3e68cdd
lahirutech <rmllbrathnayaka@gmail.com>
2022-08-18 14:47:28 +0530

    modifying the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,13 @@
 import Link from "next/link";
 import styles from "../styles/Navbar.module.css";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const cart = useSelector((state) => state.cart);
+  const getItemsCount = () => {
+    return cart.reduce((accumulator, item) => accumulator + item.quantity, 0);
+  };
+
   return (
     <nav className={styles.navbar}>
       <h6 className={styles.logo}>GamesKart</h6>
@@ -13,7 +19,9 @@ const Navbar = () => {
           <Link href="/shop">Shop</Link>
         </li>
         <li className={styles.navlink}>
-          <Link href="/cart">Cart</Link>
+          <Link href="/cart">
+            <p>Cart ({getItemsCount()})</p>
+          </Link>
         </li>
       </ul>
     </nav>
